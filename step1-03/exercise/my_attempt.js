b30d7af658c0947509fe291cff3834a7ac9ab6ee
Mark <markjohnson@bath.edu>
2019-04-27 09:56:28 +0100

    Completed javascript example

---
+++ step1-03/exercise/my_attempt.js
@@ -0,0 +1,37 @@
+function getFavs() {
+  let favList = [];
+  const inputs = document.querySelectorAll('input');
+  for (const input of inputs) {
+    if (input.checked === true) {
+      console.log('hi');
+      favList.push(input.parentNode.textContent);
+    }
+  }
+
+  console.log(favList);
+
+  const favorites = document.querySelector('.favorites');
+  favorites.textContent = favList.join(' ');
+}
+
+const button = document.querySelector('button');
+
+button.onclick = getFavs();
+
+/*
+Their answer
+
+function getFavs() {
+  let favList = [];
+  const inputs = document.querySelectorAll('input');
+  for (const input of inputs) {
+    if (input.checked === true) {
+      favList.push(input.parentNode.textContent);
+    }
+  }
+  document.querySelector('.favorites').textContent = favList.join(' ');
+}
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', getFavs);*/
